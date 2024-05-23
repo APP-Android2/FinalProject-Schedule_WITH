0be32ca80deb43d4c0ce359a565f22f6e7d21cc2
soon <soon@soonui-MacBookPro.local>
2024-05-23 11:09:09 +0900

    creat project

---
+++ android/app/src/main/kotlin/co/kr/schedule_with/schedule_with/MainActivity.kt
@@ -0,0 +1,5 @@
+package co.kr.schedule_with.schedule_with
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ui/schedule/view/view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyScheduleView extends StatefulWidget {
+  const MyScheduleView({super.key});
+
+  @override
+  State<MyScheduleView> createState() => _MyScheduleViewState();
+}
+
+class _MyScheduleViewState extends State<MyScheduleView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
