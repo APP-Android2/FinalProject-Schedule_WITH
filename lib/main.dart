0694966016a10b6aa03f17888b00cbb756289280
joy293 <opendoor2026@gmail.com>
2024-05-28 10:52:05 +0900

    💄 Add sfcalendar 라이브러리
    캘린더 라이브러리 사용할 수 있도록 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:schedule_with/ui/login/view/login_find_fail_id.dart';
@@ -7,9 +8,9 @@ import 'package:schedule_with/ui/login/view/login_join.dart';
 import 'package:schedule_with/ui/login/view/login_main.dart';
 import 'package:schedule_with/ui/login/view/login_password_reset.dart';
 import 'package:schedule_with/ui/login/view/login_password_reset_done.dart';
-import 'package:schedule_with/ui/login/view/login_read_terms.dart';
+import 'package:schedule_with/ui/schedule/view/schedule_main.dart';
 
-void main(){
+void main() {
   runApp(ScheduleWith());
 }
 
@@ -24,19 +25,23 @@ class _ScheduleWithState extends State<ScheduleWith> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+
       // 우상단 디버그 표시 제거
       debugShowCheckedModeBanner: false,
+      // 테마
       theme: ThemeData(
+        textSelectionTheme:
         // 커서 포인트 색상 변경
-        textSelectionTheme: TextSelectionThemeData(
-          selectionHandleColor: Colors.transparent
-        ),
-        primarySwatch: Colors.blue
+        TextSelectionThemeData(selectionHandleColor: Colors.transparent),
+        primarySwatch: Colors.blue,
+        // 배경 컬러
+        scaffoldBackgroundColor: Colors.white,
+
       ),
       home: Container(
         child: Scaffold(
           // 나중에 홈 화면으로 변경해야 됨
-          body: LoginMain(),
+          body: ScheduleMain(),
         ),
       ),
       // Name을 지정하여 페이지 이동
@@ -48,7 +53,7 @@ class _ScheduleWithState extends State<ScheduleWith> {
         GetPage(name: '/findFailId', page: () => LoginFindFailId()),
         GetPage(name: '/passwordReset', page: () => LoginPasswordReset()),
         GetPage(name: '/passwordResetDone', page: () => LoginPasswordResetDone()),
-        GetPage(name: '/readTerms', page: () => LoginReadTerms()),
+        GetPage(name: '/readTerms', page: () => ScheduleMain()),
       ],
     );
   }
