75375d716262a4a3262283786766306774a495e1
joy293 <opendoor2026@gmail.com>
2024-06-02 20:28:40 +0900

    💄 Add 수정하기/삭제하기 버튼
    수정하기 삭제하기 버튼 2개 세트로 묶인 UI Related by: #24

---
+++ lib/widget/main_two_button.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:schedule_with/assets/colors/color.dart';
+
+class MainTwoButton extends StatelessWidget {
+  // 왼쪽 버튼 매개변수
+  final String leftText;
+  final Color leftColor;
+  final VoidCallback? leftOnPressed;
+  // 오른쪽 버튼 매개변수
+  final String rightText;
+  final Color rightColor;
+  final VoidCallback? rightOnPressed;
+
+  const MainTwoButton(
+      {required this.leftText,
+        required this.leftColor,
+        required this.leftOnPressed,
+        required this.rightText,
+        required this.rightColor,
+        required this.rightOnPressed,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+        child: Row(
+          children: [
+            SizedBox(
+            height: 45,
+            width: 180,
+            child: TextButton(
+              onPressed: leftOnPressed,
+              child: Text(leftText,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  )),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return Colors.white;
+                    }
+                    return leftColor;
+                  },
+                ),
+                foregroundColor:
+                    MaterialStateProperty.resolveWith<Color>((states) {
+                  if (states.contains(MaterialState.disabled)) {
+                    return mainOrange;
+                  }
+                  return Colors.white;
+                }),
+                side: MaterialStateProperty.resolveWith<BorderSide>(
+                  (states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return BorderSide(color: mainOrange);
+                    }
+                    return BorderSide.none;
+                  },
+                ),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            )),
+        // 버튼 사이 여백
+            Spacer(),
+            SizedBox(
+            height: 45,
+            width: 180,
+            child: TextButton(
+              onPressed: rightOnPressed,
+              child: Text(rightText,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  )),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return Colors.white;
+                    }
+                    return rightColor;
+                  },
+                ),
+                foregroundColor:
+                    MaterialStateProperty.resolveWith<Color>((states) {
+                  if (states.contains(MaterialState.disabled)) {
+                    return mainOrange;
+                  }
+                  return Colors.white;
+                }),
+                side: MaterialStateProperty.resolveWith<BorderSide>(
+                  (states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return BorderSide(color: mainOrange);
+                    }
+                    return BorderSide.none;
+                  },
+                ),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            )
+        )
+      ],
+    )
+    );
+  }
+}
